f9ea37c1a9f16a5866b65527c4d43c77cf04982a
tarekbinjaman <tarekbinjaman@gmail.com>
2026-05-03 16:51:24 +0600

    Authprovider setup done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/Navbar/Navbar";
-import AuthProvider from "../services/authService"
+import AuthProvider from "../context/AuthContext"
 
 const outfit = Outfit({
   subsets: ["latin"],
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -3,44 +3,49 @@ import { getMe } from "../services/authService";
 
 const AuthContext = createContext();
 
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-const AuthProvider = ({children}) => {
-    const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(true);
+  // fetch user from backend
+  const fetchUser = async () => {
+    try {
+      const data = await getMe();
+      setUser(data);
+    } catch (err) {
+      console.log(err.message);
+      setUser(null);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    // fetch user from backend
-    const fetchUser = async () => {
-        try {
-            const data = await getMe();
-            setUser(data);
-        } catch (err) {
-            console.log(err.message)
-            setUser(null)
-        } finally {
-            setLoading(false);
-        }
+  // run on app load
+  useEffect(() => {
+    const loadUser = async () => {
+      await fetchUser();
     };
+    loadUser();
+  }, []);
 
-    // run on app load
-    useEffect(() => {
-        fetchUser();
-    }, [])
+  // setting up user data by setAuth
+  const setAuth = (authUser) => {
+    setUser(authUser);
+  };
 
-    // setting up user data by setAuth
-    const setAuth = (authUser) => {
-        setUser(authUser)
-    };
+  // clearing user data
+  const clearAuth = () => {
+    setUser(null);
+  };
 
-    // clearing user data 
-    const clearAuth = () => {
-        setUser(null)
-    };
-
-    return <AuthContext.Provider value={user, loading, setAuth, clearAuth, fetchUser}>
-        {children}
+  return (
+    <AuthContext.Provider
+      value={{ user, loading, setAuth, clearAuth, fetchUser }}
+    >
+      {children}
     </AuthContext.Provider>
-
-}
+  );
+};
 
 // hook
 export const useAuth = () => useContext(AuthContext);
