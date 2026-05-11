21abc43398c2b85855a923f8aad3b65949bc199c
tarekbinjaman <tarekbinjaman@gmail.com>
2026-05-12 01:48:55 +0600

    dashboard admin, user, tutor links added

--- src/components/dashboard/appsidebar.tsx
+++ src/components/dashboard/appsidebar.tsx
@@ -1,11 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import {
-  LayoutDashboard,
-  User,
-  CalendarCheck,
-} from "lucide-react";
+import { LayoutDashboard, User, CalendarCheck } from "lucide-react";
 
 import {
   Sidebar,
@@ -16,15 +12,106 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import { useAuth } from "@/src/context/AuthContext";
 
 export function AppSidebar() {
+  const { user } = useAuth();
+
+  const studentLinks = [
+    {
+      title: "Student Dashboard",
+      href: "/dashboard/Students/dashboardOverview",
+      icon: LayoutDashboard,
+    },
+    {
+      title: "Edit Profile",
+      href: "/dashboard/Students/editProfile",
+      icon: LayoutDashboard,
+    },
+    {
+      title: "My Booking",
+      href: "/dashboard/Students/mybooking",
+      icon: LayoutDashboard,
+    },
+    {
+      title: "Book a Session",
+      href: "/dashboard/Students/bookSession",
+      icon: LayoutDashboard,
+    },
+  ];
+
+  const tutorLinks = [
+    {
+      title: "Tutor Dashboard",
+      href: "/dashboard/tutor/dashboardOverview",
+      icon: LayoutDashboard,
+    },
+    {
+      title: "Edit Profile",
+      href: "/dashboard/tutor/editProfile",
+      icon: LayoutDashboard,
+    },
+    {
+      title: "My Sessions",
+      href: "/dashboard/tutor/mySession",
+      icon: LayoutDashboard,
+    },
+    {
+      title: "Set Availability",
+      href: "/dashboard/tutor/setAvailability",
+      icon: LayoutDashboard,
+    },
+    {
+      title: "View Reviews",
+      href: "/dashboard/tutor/viewReviews",
+      icon: LayoutDashboard,
+    },
+  ];
+  
+
+  const adminLinks = [
+    {
+      title: "Admin Dashboard",
+      href: "/dashboard/admin/dashboardOverview",
+      icon: LayoutDashboard,
+    },
+    {
+      title: "Manage Users",
+      href: "/dashboard/admin/manageUsers",
+      icon: LayoutDashboard,
+    },
+    {
+      title: "All Bookings",
+      href: "/dashboard/admin/allBooking",
+      icon: LayoutDashboard,
+    },
+    {
+      title: "Manage Categories",
+      href: "/dashboard/admin/manageCategories",
+      icon: LayoutDashboard,
+    },
+  ];
+
+
+  let links = [];
+
+  if(user?.role === "STUDENT") {
+    links = studentLinks;
+  }
+
+  if(user?.role === "TUTOR") {
+    links = tutorLinks;
+  }
+
+  if(user?.role === "ADMIN") {
+    links = adminLinks;
+  }
+
   return (
     <Sidebar>
       <SidebarContent>
         <SidebarGroup>
-          <SidebarGroupLabel>
-            Dashboard
-          </SidebarGroupLabel>
+          <SidebarGroupLabel>Dashboard</SidebarGroupLabel>
 
           <SidebarMenu>
             <SidebarMenuItem>
@@ -35,7 +122,43 @@ export function AppSidebar() {
                 </Link>
               </SidebarMenuButton>
             </SidebarMenuItem>
+
             <SidebarMenuItem>
+              <SidebarMenuButton asChild>
+                <Link href="/">
+                  <LayoutDashboard />
+                  <span>Home</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+
+            {/* user buttons */}
+
+          
+          {
+            links.map((link)=> (
+              <SidebarMenuItem key={link.href}>
+                <SidebarMenuButton asChild>
+                  <Link href={link.href}>
+                  <link.icon />
+                  <span>{link.title}</span>
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            ))
+          }
+
+
+
+
+
+
+
+
+
+
+
+            {/* <SidebarMenuItem>
               <SidebarMenuButton asChild>
                 <Link href="/">
                   <LayoutDashboard />
@@ -78,7 +201,12 @@ export function AppSidebar() {
                   <span>Bookings</span>
                 </Link>
               </SidebarMenuButton>
-            </SidebarMenuItem>
+            </SidebarMenuItem> */}
+
+
+
+
+
           </SidebarMenu>
         </SidebarGroup>
       </SidebarContent>
