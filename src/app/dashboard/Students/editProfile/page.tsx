5ea9c570424fb54e67c7f39b1899d67658136f26
tarekbinjaman <tarekbinjaman@gmail.com>
2026-05-12 16:07:06 +0600

    working on user edit profile page

--- src/app/dashboard/Students/editProfile/page.tsx
+++ src/app/dashboard/Students/editProfile/page.tsx
@@ -1,4 +1,9 @@
-export default function editProfile() {
+'use client'
+
+import { useAuth } from "@/src/context/AuthContext"
+
+export default function EditProfile() {
+    const {user} = useAuth();
     return (
         <div>
             <h1>This is user edit profile route</h1>
