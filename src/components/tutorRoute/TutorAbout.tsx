f7bebf8d5abf1e5f3d202bc03befbc2976e36445
tarekbinjaman <tarekbinjaman@gmail.com>
2026-08-10 00:07:12 +0600

    working on single tuttor route

--- src/components/tutorRoute/TutorAbout.tsx
+++ src/components/tutorRoute/TutorAbout.tsx
@@ -1,18 +1,38 @@
+"use client";
+
+import { Info, User } from "lucide-react";
+import { useState } from "react";
+
 type Props = {
   tutor: any;
 };
 
 export default function TutorAbout({ tutor }: Props) {
-  return (
-    <section className="rounded-2xl border bg-white p-8 shadow-sm h-[238px] overflow-y-scroll">
-      <h2 className="mb-4 text-2xl font-bold">
-        About Tutor
-      </h2>
+  const [showMore, setShowMore] = useState(false);
 
-      <p className="leading-8 text-gray-600">
-        {tutor.bio ||
-          "This tutor hasn't added a bio yet."}
+  return (
+    <section className="bg-white p-8 rounded-2xl border">
+      <div className="flex items-center mb-4 gap-2">
+      <User className="text-blue-400 bg-blue-100 h-10 w-10 p-2 rounded-full" />
+      <h2 className="text-2xl font-bold">About Tutor</h2>
+      </div>
+
+      <p
+        className={`leading-8 text-gray-600 ${
+          !showMore ? "line-clamp-4" : ""
+        }`}
+      >
+        {tutor.bio || "This tutor hasn't added a bio yet."}
       </p>
+
+      {tutor.bio && (
+        <button
+          onClick={() => setShowMore(!showMore)}
+          className="mt-2 font-medium text-blue-600 hover:underline"
+        >
+          {showMore ? "See less" : "See more"}
+        </button>
+      )}
     </section>
   );
 }
